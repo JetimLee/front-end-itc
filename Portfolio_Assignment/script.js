afc07d86210f4a30a30b05c6684e97739af8a472
jetimlee <gavincoulson1@gmail.com>
2021-10-10 22:02:54 +0300

    just need to add a video for the assignment, won'ta ctually use that in the portfolio.

--- Portfolio_Assignment/script.js
+++ Portfolio_Assignment/script.js
@@ -3,6 +3,7 @@ console.log("working");
 const hamburger = document.getElementById("hamburger");
 const backdrop = document.getElementById("backdrop");
 const sidebar = document.getElementById("sidebar");
+const profilePic = document.getElementById("sideBarPortfolioPicture");
 
 const toggleSidebar = () => {
   backdrop.classList.toggle("backdrop--active");
@@ -15,3 +16,12 @@ hamburger.addEventListener("click", () => {
 backdrop.addEventListener("click", () => {
   toggleSidebar();
 });
+
+const getUserData = async (url) => {
+  const request = await fetch(url);
+  const data = await request.json();
+  profilePic.src = data.avatar_url;
+  profilePic.style.border = "1px solid black";
+};
+
+getUserData("https://api.github.com/users/JetimLee");
