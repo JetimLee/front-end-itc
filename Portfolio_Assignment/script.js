54ae2e993f300a1e7e051c07c12923c59ce0c1e1
jetimlee <gavincoulson1@gmail.com>
2021-10-11 11:37:22 +0300

    made some cool typing text and a great hero section.

--- Portfolio_Assignment/script.js
+++ Portfolio_Assignment/script.js
@@ -4,6 +4,7 @@ const hamburger = document.getElementById("hamburger");
 const backdrop = document.getElementById("backdrop");
 const sidebar = document.getElementById("sidebar");
 const profilePic = document.getElementById("sideBarPortfolioPicture");
+const backgroundPicture = document.getElementById("backgroundImg");
 
 const toggleSidebar = () => {
   backdrop.classList.toggle("backdrop--active");
@@ -20,6 +21,7 @@ backdrop.addEventListener("click", () => {
 const getUserData = async (url) => {
   const request = await fetch(url);
   const data = await request.json();
+  // backgroundPicture.src = data.avatar_url;
   profilePic.src = data.avatar_url;
   profilePic.style.border = "1px solid black";
 };
