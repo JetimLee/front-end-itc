9871462ce1a44e2134c242b7029cd4e5182c2109
jetimlee <gavincoulson1@gmail.com>
2021-10-21 17:12:25 +0300

    got into when type reference doesn't work, pretty useful examples.

--- TypeScript_Stephen_Grider/annotations/anyType.ts
+++ TypeScript_Stephen_Grider/annotations/anyType.ts
@@ -9,10 +9,20 @@ console.log(coordinates);
 //any type means TS has no idea what is going on, so it won't be able to check for correct property references
 //type inference only works when you do variable declaration and initialization on the same line
 let words: string[] = ["red", "green", "blue"];
-let foundWord: boolean;
+let foundWord: boolean = false;
 for (let i = 0; i < words.length; i++) {
   if (words[i] === "green") {
     foundWord = true;
   }
 }
 console.log(foundWord);
+
+//3 Variable whose type cannot be inferred correctly
+let myNumbers: number[] = [1, 2, 3, -10, -20];
+let numberAbovezero: boolean | number = false;
+for (let i = 0; i < myNumbers.length; i++) {
+  if (myNumbers[i] > 0) {
+    numberAbovezero = myNumbers[i];
+  }
+}
+console.log(numberAbovezero);
