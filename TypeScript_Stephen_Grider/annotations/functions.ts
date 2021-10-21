7759a889f338651e7415e587e29bcbe58a639a02
jetimlee <gavincoulson1@gmail.com>
2021-10-21 16:46:08 +0300

    use inference when possible, otherwise do annotation to avoid the any type.

--- TypeScript_Stephen_Grider/annotations/functions.ts
+++ TypeScript_Stephen_Grider/annotations/functions.ts
@@ -1,5 +1,7 @@
 //expecting to get nothing from this function so I am setting my expectations to void
-const logNumber: (i:number) => void = (i:number) => {
-console.log(i);
+const logNumber: (i: number) => void = (i: number) => {
+  console.log(i);
 };
-logNumber(8)
+logNumber(8);
+
+//use annotation to avoid the any type, otherwise us TS reference
--- TypeScript_Stephen_Grider/annotations/variables.ts
+++ TypeScript_Stephen_Grider/annotations/variables.ts
@@ -1,8 +1,9 @@
 //typescript can infer the type at times - annotations are the types directly tell to typescript
 let apples = 5;
-let speed: string = 'fast'
+let speed: string = "fast";
 let hasName: boolean = true;
 let nothingMuch: null = null;
 let nothing: undefined = undefined;
-let mySymbol:symbol = Symbol('hi there');
+let mySymbol: symbol = Symbol("hi there");
 let now: Date = new Date();
+let newThing: string = "hello";
