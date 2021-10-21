ef7c4781daa4ffc388cb214fdae5861c7172025c
jetimlee <gavincoulson1@gmail.com>
2021-10-21 16:40:57 +0300

    learning about type inference vs type annotation. annotation is what the developer tells to typescript, inference is what typescript infers.

---
+++ TypeScript_Stephen_Grider/annotations/functions.ts
@@ -0,0 +1,5 @@
+//expecting to get nothing from this function so I am setting my expectations to void
+const logNumber: (i:number) => void = (i:number) => {
+console.log(i);
+};
+logNumber(8)
--- TypeScript_Stephen_Grider/annotations/variables.ts
+++ TypeScript_Stephen_Grider/annotations/variables.ts
@@ -1,4 +1,5 @@
-let apples: number = 5;
+//typescript can infer the type at times - annotations are the types directly tell to typescript
+let apples = 5;
 let speed: string = 'fast'
 let hasName: boolean = true;
 let nothingMuch: null = null;
