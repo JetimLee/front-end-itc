7f216c225361200ff4f040fe7af3299462803dac
jetimlee <gavincoulson1@gmail.com>
2021-10-21 13:09:34 +0300

    first time using type script to check types

--- TypeScript_Stephen_Grider/first_typescript_app/index.ts
+++ TypeScript_Stephen_Grider/first_typescript_app/index.ts
@@ -2,6 +2,25 @@ import axios from 'axios'
 
 const url = 'https://jsonplaceholder.typicode.com/comments/1'
 
+//interfaces are used to describe the structure of an object
+//can ignore properties if wanted/needed
+interface Comment {
+   postId: number;
+   id: number;
+   name: string;
+   email: string;
+   body: string;
+}
+
 axios.get(url).then(response=>{
-    console.log(response.data)
+   //response.data has properties of:
+   //list properties
+   const comment = response.data as Comment;
+   const id = comment.id
+   const postId = comment.postId
+   const name = comment.name
+   const email = comment.email
+   const body = comment.body
+   console.log(`The body is ${body}`)
+   
 })
