8795b324ab5bbab35e2f6b722e005d2bb1b7a745
jetimlee <gavincoulson1@gmail.com>
2021-10-24 11:14:32 +0300

    got to milestone 2.

--- project_1/src/components/Container.js
+++ project_1/src/components/Container.js
@@ -4,22 +4,60 @@ import Card from "./Card";
 import NoteCard from "./NoteCard";
 
 const Container = () => {
+  let deleteNote = (value) => {
+    console.log("deleting");
+    let question = window.confirm("Are you sure you want to delete?");
+    if (question) {
+      let noteToDelete = notes.splice(value, 1);
+      setNotes(
+        notes.filter((el) => {
+          return el !== noteToDelete;
+        })
+      );
+    }
+  };
   let handleChange = (e) => {
     setNote(e.target.value);
   };
+  let convertDate = (dateTimeInMiliseconds) => {
+    let fullDate = new Date(dateTimeInMiliseconds).toLocaleString("en-US", {
+      timeZone: "Israel",
+    });
+    console.log(fullDate);
+    return fullDate;
+  };
+
+  let current = new Date().getTime();
   let submitHandler = () => {
     setNotes([note, ...notes]);
-    console.log("this is notes");
-    console.log(notes);
+    setDate(current);
+    setDates([...dates, current]);
+    // console.log("this is notes");
+    // console.log(notes);
   };
+
   const [note, setNote] = useState("");
   const [notes, setNotes] = useState([]);
+  const [date, setDate] = useState({});
+  const [dates, setDates] = useState([]);
+  console.log("this is dates");
+  console.log(dates);
+  console.log("this is date");
+  console.log(date);
   return (
     <div className="container">
       <Card submitHandler={submitHandler} handleChange={handleChange} />
       <div className="noteCardContainer">
         {notes.map((el, i) => {
-          return <NoteCard text={notes[i]} key={i} />;
+          return (
+            <NoteCard
+              deleteNote={deleteNote}
+              text={notes[i]}
+              id={i}
+              key={i}
+              dateProp={convertDate(dates[i])}
+            />
+          );
         })}
       </div>
     </div>
--- project_1/src/components/NoteCard.js
+++ project_1/src/components/NoteCard.js
@@ -1,10 +1,14 @@
 import React from "react";
-import "./NoteCard.module.css";
 
 const NoteCard = (props) => {
+  console.log(props);
   return (
     <div className="note__card">
-      <p>{props.text}</p>
+      <h1>Title: </h1>
+      <p>Note: {props.text}</p>
+      <p>ID: {props.id}</p>
+      <p>Date created: {props.dateProp}</p>
+      <button onClick={() => props.deleteNote(props.id)}>Delete</button>
     </div>
   );
 };
