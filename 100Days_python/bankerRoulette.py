8f91abce3c82a64d612a52be753494380e70963d
jetimlee <gavincoulson1@gmail.com>
2021-11-28 10:29:16 +0200

    did a banker roulette challenge. quite fun.

---
+++ 100Days_python/bankerRoulette.py
@@ -0,0 +1,12 @@
+import random
+
+
+def banker_roulette():
+    names = input("Give me all of the names, separated by a comma")
+    names_list = names.split(',')
+    random_number = random.randint(0, len(names_list))
+    person_to_pay = names_list[random_number]
+    print(f'The person that pays is {person_to_pay}')
+
+
+banker_roulette()
