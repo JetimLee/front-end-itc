9562d48263d34dce1d22ffb4d70bcbd186b02676
jetimlee <gavincoulson1@gmail.com>
2021-12-12 22:52:04 +0200

    did some cool stuff with ahngman and what not.

--- 100Days_python/passwordGenerator.py
+++ 100Days_python/passwordGenerator.py
@@ -15,17 +15,17 @@ def create_password():
     password_array = []
     for letter in range(number_of_letters):
         chosen_letter = alphabet[random.randint(0, 46)]
+        # could also do random.choice instead of passing in a random number as the index
         password_array.append(chosen_letter)
     # print(password_array)
     for number in range(number_of_numbers):
-        chosen_number = numbers[random.randint(0, number)*3]
+        chosen_number = numbers[random.randint(0, number)]
         password_array.append(chosen_number)
     # print(password_array)
     for symbol in range(number_of_symbols):
         chosen_symbol = symbols[random.randint(0, number_of_symbols)]
         password_array.append(chosen_symbol)
     password = random.sample(password_array, len(password_array))
-    password
     actual_password = (''.join(password))
     print(f'Your password is {actual_password}')
 
