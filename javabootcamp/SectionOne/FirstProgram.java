848f314c5d8e6451796553265073dc22db52ba3c
Gavin Coulson <gavincoulson@Gavins-MacBook-Air.local>
2021-12-24 07:54:40 -0700

    started java course

---
+++ javabootcamp/SectionOne/FirstProgram.java
@@ -0,0 +1,18 @@
+// TASK 1: Make a class.
+
+public class FirstProgram{
+    public static void main(String[] args) {
+        System.out.println("Java > Python");
+        System.out.println("I spilled Java all over my paper.");
+        System.out.println("My dog ate my Java.");
+
+    }
+}
+
+// TASK 2: Inside the class, write the main() method.
+
+
+// TASK 3: Print these messages to the console: "Printing is fun!" 
+                                              // "Java > Python."
+                                              // "I spilled Java all over my paper."
+                                              // "My dog ate my Java." 
