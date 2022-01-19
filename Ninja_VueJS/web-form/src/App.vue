810483ef4223ff847658a36a0401671891952d1f
jetimlee <gavincoulson1@gmail.com>
2022-01-19 22:55:08 +0200

    ninja

---
+++ Ninja_VueJS/web-form/src/App.vue
@@ -0,0 +1,18 @@
+<template></template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+body {
+  background: #eee;
+}
+</style>
