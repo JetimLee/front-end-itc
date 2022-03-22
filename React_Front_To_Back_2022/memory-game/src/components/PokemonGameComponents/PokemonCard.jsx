428f7a3cae1c3501a0e367c707c62a53bfacb616
unknown <gavincoulson1@gmail.com>
2022-03-22 19:58:20 -0400

    made a hash map that watches for repeat pokemon cards being put into the selected cards.

--- React_Front_To_Back_2022/memory-game/src/components/PokemonGameComponents/PokemonCard.jsx
+++ React_Front_To_Back_2022/memory-game/src/components/PokemonGameComponents/PokemonCard.jsx
@@ -4,17 +4,33 @@ import { MemoryCardProvider } from "../../context/MemoryCardContext";
 import MemoryCardContext from "../../context/MemoryCardContext";
 
 import "../../component_styling/PokemonCard.css";
-//move this to the parent component? may not be necessary with context...
+//need to check for repeats in the array - if so, player loses and game is over
+//need to create a has won/loss state. loss will record the score, won will result in a new list of pokemon
+//need to then see if the selectedCards array is equal to the cards array
 const PokemonCard = ({ pokemonObj }) => {
-  const { selectedCards, setSelectedCards } = useContext(MemoryCardContext);
-  const selectedCard = (pokemonObj) => {
+  const { selectedCards, setSelectedCards, cards } =
+    useContext(MemoryCardContext);
+  const selectCard = (pokemonObj) => {
     console.log("clicked");
-    setSelectedCards([...selectedCards, pokemonObj]);
-    console.log(selectedCards);
+    const updatedSelectedCards = [...selectedCards, pokemonObj];
+    setSelectedCards(updatedSelectedCards);
+    console.log("updated selected cards", updatedSelectedCards);
+
+    //create hash map here to track for repeat cards selected
+    let pokemonMap = {};
+    for (let i = 0; i < updatedSelectedCards.length; i++) {
+      console.log(updatedSelectedCards[i]);
+      if (pokemonMap.hasOwnProperty(updatedSelectedCards[i].name)) {
+        console.log("YOU LOSE!!!");
+      } else {
+        pokemonMap[updatedSelectedCards[i].name] = updatedSelectedCards[i].name;
+        console.log("pokemon map", pokemonMap);
+      }
+    }
   };
   const { image, name } = pokemonObj;
   return (
-    <div onClick={() => selectedCard(pokemonObj)} className="card-container">
+    <div onClick={() => selectCard(pokemonObj)} className="card-container">
       <div className="card">
         <img src={image} alt={"a pokemion card"} />
         <p>{name}</p>
