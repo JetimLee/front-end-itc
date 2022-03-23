b5c8e7867eedb51e8eabc62ad1f00502588df674
unknown <gavincoulson1@gmail.com>
2022-03-22 20:02:16 -0400

    added a bunch of todos for tomorrow and the overall path for the app. making good progress.

--- React_Front_To_Back_2022/memory-game/src/context/MemoryCardContext.js
+++ React_Front_To_Back_2022/memory-game/src/context/MemoryCardContext.js
@@ -3,13 +3,19 @@ import { createContext, useState } from "react";
 const MemoryCardContext = createContext();
 
 export const MemoryCardProvider = ({ children }) => {
+  //added three pieces of state here - has lost, has won and is playing
+  //need to conditionally render in the app if the player is playing or not
+  //need to make new functionality that if the player wins then a new api is called and more pokemon are loaded
+  //displayed list needs to be shuffled each time the list is rendered
+  //if player loses scoreboard component is displayed
+
+  const [hasWon, setHasWon] = useState(false);
+  const [hasLost, setHasLost] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
   const [cards, setCards] = useState([]);
   const [selectedCards, setSelectedCards] = useState([]);
   const [pokemonAmount, setPokemonAmount] = useState(12);
 
-  const handleClick = (e) => {
-    console.log(e);
-  };
   return (
     <MemoryCardContext.Provider
       value={{
@@ -19,7 +25,6 @@ export const MemoryCardProvider = ({ children }) => {
         cards,
         selectedCards,
         pokemonAmount,
-        handleClick,
       }}
     >
       {children}
