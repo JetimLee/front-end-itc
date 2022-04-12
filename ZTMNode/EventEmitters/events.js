8857181e8d2aeea0bbf604769cec107e91c1c361
unknown <gavincoulson1@gmail.com>
2022-04-11 20:23:49 -0400

    cool stuff with events in node

---
+++ ZTMNode/EventEmitters/events.js
@@ -0,0 +1,20 @@
+const { profileEnd } = require("console");
+const EventEmitter = require("events");
+//creating observers in nodejs
+const gavin = new EventEmitter();
+
+//can also pass these different parameters like results
+gavin.on("gavin is the best", () => {
+  console.log("gavin is the best!");
+});
+
+gavin.on("gavin is the best", () => {
+  console.log("yes he is!");
+});
+
+//proceess needs to be watched before the emit occurs, otherwise the process won't be caught
+process.on("exit", (code) => {
+  console.log(`Process finished with an exit code of ${code}`);
+});
+
+gavin.emit("gavin is the best");
