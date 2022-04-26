355bbbdfe4a9d4e5bdc3cb0cd5ca4c17ee3a6896
= <gavincoulson1@gmail.com>
2022-04-26 19:12:44 -0400

    make some custom scripts and setup the node server to serve the application now. cool stuff, good review.

--- ZTMNode/NASA API/server/src/server.js
+++ ZTMNode/NASA API/server/src/server.js
@@ -1,14 +1,22 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const planetsRouter = require("./routes/planets/planets.router");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const app = express();
-
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
 app.use("/", planetsRouter);
+app.get("/", (req, resp) => {
+  resp.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+//check this - should be the catch all
+app.get("*", (req, resp) => {
+  resp.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 const port = process.env.PORT || 8000;
 
