2e30128a965dc5d8dfdd34d6a7b86727b3c7af6d
= <gavincoulson1@gmail.com>
2022-05-17 21:51:26 -0400

    had fun with revisitng the fundamentals of promises.

---
+++ JavaScript Fundamentals/05_Callback_Hell/app.js
@@ -0,0 +1,40 @@
+const btn = document.querySelector("button");
+
+//This function moves an element "amount" number of pixels after a delay.
+//If the element will stay on screen, we move the element and call the onSuccess callback function
+//If the element will move off screen, we do not move the element and instead call the onFailure callback
+const moveX = (element, amount, delay, onSuccess, onFailure) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const bodyBoundary = document.body.clientWidth;
+      const elRight = element.getBoundingClientRect().right;
+      const currLeft = element.getBoundingClientRect().left;
+      if (elRight + amount > bodyBoundary) {
+        onFailure();
+        reject();
+      } else {
+        element.style.transform = `translateX(${currLeft + amount}px)`;
+        onSuccess();
+        resolve();
+      }
+    }, delay);
+  });
+};
+const success = () => {
+  alert("You have a wide screen!");
+};
+const failure = () => {
+  alert("You have a narrow screen!");
+};
+
+const moveButton = async () => {
+  for (let i = 0; i < 10; i++) {
+    try {
+      await moveX(btn, 100, 1000, success, failure);
+    } catch (error) {
+      return;
+    }
+  }
+  alert("YOU HAVE A VERY WIDE SCREEN!");
+};
+moveButton();
