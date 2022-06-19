fe0a5acb1b1f4733d3e31517365b491ca7c6ca6d
Gavin Coulson <gavincoulson1@gmail.com>
2022-06-19 14:20:09 -0400

    figured out how to use and provide context with typescript. that was a trip.:

--- React_Front_To_Back_2022/github-profile-finder/src/context/github/GithubContext.tsx
+++ React_Front_To_Back_2022/github-profile-finder/src/context/github/GithubContext.tsx
@@ -1,17 +1,45 @@
-import React, { createContext, FC } from "react";
+import React, { createContext, FC, useState, ReactNode } from "react";
 import { user } from "../../interfaces/userInterface";
 
 interface GithubContextInterface {
-  children?: React.ReactNode;
+  userList: user[];
+  loading: boolean;
+  getUsers: Function;
+}
+interface Props {
+  children?: ReactNode;
 }
 
-const GithubContext = createContext<GithubContextInterface | null>(null);
+export const GithubContext = createContext<GithubContextInterface | null>(null);
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
 const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
-export const GithubProvider: FC<GithubContextInterface> = ({ children }) => {
-  return <GithubContext.Provider value={{}}>{children}</GithubContext.Provider>;
+export const getUsers = async (): Promise<user[] | Error> => {
+  const data: Response = await fetch(`${GITHUB_URL}/users`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`,
+    },
+  });
+  try {
+    const response: user[] = await data.json();
+    console.log(response);
+    return response;
+  } catch (error) {
+    console.log(error);
+    return error as Error;
+  }
+};
+
+export const GithubProvider = ({ children }: Props) => {
+  const [userList, setUserList] = useState<user[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  return (
+    <GithubContext.Provider value={{ loading, userList, getUsers }}>
+      {children}
+    </GithubContext.Provider>
+  );
 };
 
 //  return <TodoContext.Provider value={{ todos, saveTodo, updateTodo }}>{children}</TodoContext.Provider>;
