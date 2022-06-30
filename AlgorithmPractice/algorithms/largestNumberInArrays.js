db0d8549447c060f235a129cd3ba3dd580a0a460
JetimLee <gavincoulson1@gmail.com>
2022-06-30 11:55:26 -0400

    fun stuff

--- AlgorithmPractice/algorithms/largestNumberInArrays.js
+++ AlgorithmPractice/algorithms/largestNumberInArrays.js
@@ -1,5 +1,21 @@
 function largestOfFour(arr) {
-  return arr;
+  let results = [];
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    let max = arr[i];
+    console.log(max, "new array");
+    let biggestNumber = arr[i][0];
+    for (let j = 0; j < arr.length; j++) {
+      console.log(arr[i][j]);
+      if (arr[i][j] > biggestNumber) {
+        biggestNumber = arr[i][j];
+      }
+    }
+    results.push(biggestNumber);
+  }
+  console.log(results);
+  return results;
 }
 
 largestOfFour([
