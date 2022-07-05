a61063b6ec7c35e57ea99a62c930b0613b1e1c07
JetimLee <gavincoulson1@gmail.com>
2022-07-05 12:05:54 -0400

    morefun

--- AlgorithmPractice/algorithms/truncateString.js
+++ AlgorithmPractice/algorithms/truncateString.js
@@ -1,5 +1,33 @@
 function truncateString(str, num) {
-  return str;
+  console.log(num);
+  if (str.length === num) {
+    return str;
+  }
+  if (num > str.length) {
+    return str;
+  }
+  let truncatedString = "";
+  let truncation = "...";
+
+  for (let i = 0; i < num; i++) {
+    console.log(str[i]);
+    truncatedString += str[i];
+    if (typeof str[i + 1] === "undefined") {
+      return;
+    }
+  }
+
+  return truncatedString + truncation;
 }
 
-truncateString("A-tisket a-tasket A green and yellow basket", 8);
+console.log(
+  truncateString(
+    "A-tisket a-tasket A green and yellow basket",
+    "A-tisket a-tasket A green and yellow basket".length
+  )
+);
+
+truncateString(
+  "A-tisket a-tasket A green and yellow basket",
+  "A-tisket a-tasket A green and yellow basket".length + 2
+);
