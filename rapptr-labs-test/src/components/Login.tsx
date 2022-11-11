a2b69de522df15cd55c8920b29cab97282831cf8
JetimLee <gavincoulson1@gmail.com>
2022-11-11 17:57:26 -0500

    looks good

--- rapptr-labs-test/src/components/Login.tsx
+++ rapptr-labs-test/src/components/Login.tsx
@@ -11,6 +11,8 @@ export const Login = () => {
   const dispatch = useAppDispatch()
   const emailInputRef = useRef() as MutableRefObject<HTMLInputElement>
   const passwordInputRef = useRef() as MutableRefObject<HTMLInputElement>
+  const emailErrorRef = useRef() as MutableRefObject<HTMLSpanElement>
+  const passwordErrorRef = useRef() as MutableRefObject<HTMLSpanElement>
 
   const validateEmailInput = () => {
     console.log('hello')
@@ -22,10 +24,11 @@ export const Login = () => {
     const emailRegex =
       /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     if (!emailRegex.test(emailInputRef.current.value)) {
-      emailInputRef.current.classList.add('login__input--invalid')
+      emailErrorRef.current.classList.remove('login__error--hide')
       setValidEmail(false)
     } else {
       emailInputRef.current.classList.remove('login__input--invalid')
+      emailErrorRef.current.classList.add('login__error--hide')
       setValidEmail(true)
       console.log(validEmail, 'valid email')
     }
@@ -33,10 +36,12 @@ export const Login = () => {
   const validatePasswordLength = () => {
     console.log('password')
     if (passwordInputRef.current.value.trim().length < 4) {
-      passwordInputRef.current.classList.add('login__input--invalid')
+      passwordErrorRef.current.classList.remove('login__error--hide')
+
       setValidPassword(false)
     } else {
-      passwordInputRef.current.classList.remove('login__input--invalid')
+      passwordErrorRef.current.classList.add('login__error--hide')
+
       setValidPassword(true)
       console.log(validPassword, 'valid password')
     }
@@ -56,6 +61,7 @@ export const Login = () => {
       body: JSON.stringify(formData),
       mode: 'cors',
     }
+
     const loginUrl = `http://dev.rapptrlabs.com/Tests/scripts/user-login.php`
 
     try {
@@ -66,6 +72,7 @@ export const Login = () => {
     } catch (error) {
       console.log('an error occurred', error)
     }
+    console.log(settings.body)
   }
 
   useEffect(() => {
@@ -93,6 +100,12 @@ export const Login = () => {
                 id="login_input"
                 placeholder="user@rapptrlabs.com"
               />
+              <span
+                ref={emailErrorRef}
+                className="login__error--hide login__error-message"
+              >
+                <p>Not a valid email</p>
+              </span>
             </div>
           </div>
 
@@ -107,6 +120,12 @@ export const Login = () => {
                 id="password_input"
                 placeholder="Must be at least 4 characters long"
               />
+              <span
+                ref={passwordErrorRef}
+                className="login__error--hide login__error-message"
+              >
+                <p>Invalid password</p>
+              </span>
             </div>
           </div>
           <button
