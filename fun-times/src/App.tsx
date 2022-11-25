7778db1d2585266455eb1a427701fff4ea3932be
JetimLee <gavincoulson1@gmail.com>
2022-11-25 16:14:59 -0500

    adding gql

--- fun-times/src/App.tsx
+++ fun-times/src/App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/react-in-jsx-scope */
 import './App.css'
 import { Login, TodoList } from './components'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -6,6 +7,7 @@ import { useEffect } from 'react'
 import { TodoItem } from './interfaces'
 import { useAppDispatch } from './hooks/useTypedSelector'
 import { setToDoItems } from './features/slices/todoSlice'
+import React from 'react'
 function App() {
   return (
     <div className="App">
--- fun-times/src/main.tsx
+++ fun-times/src/main.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
