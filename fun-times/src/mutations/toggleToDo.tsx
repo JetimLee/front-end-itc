ff27ef97a8e985274ad3fbc8ded8c8c0c2d484b7
JetimLee <gavincoulson1@gmail.com>
2022-11-27 16:53:36 -0500

    got the ability to toggle the todo if its done or not, now to add full crud functionality using graphql

---
+++ fun-times/src/mutations/toggleToDo.tsx
@@ -0,0 +1,13 @@
+import { gql } from '@apollo/client'
+
+export const TOGGLE_TODO = gql`
+  mutation toggleTodo($id: uuid!, $done: Boolean!) {
+    update_todos(where: { id: { _eq: $id } }, _set: { done: $done }) {
+      returning {
+        done
+        id
+        text
+      }
+    }
+  }
+`
