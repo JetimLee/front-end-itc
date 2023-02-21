b6e65d1e917f62412198499e4eaaf1469740488c
JetimLee <gavincoulson1@gmail.com>
2023-02-21 16:00:34 -0700

    clean up

--- AlgorithmPractice/practice-dynamic-array-master/dynamic-array.js
+++ AlgorithmPractice/practice-dynamic-array-master/dynamic-array.js
@@ -18,6 +18,9 @@ class DynamicArray {
     // You may not use any built-in JS array functions
     for (let i = this.data.length; i > 0; i--) {
       this.data[i] = this.data[i - 1];
+      if (this.data[i] !== undefined) {
+        this.length++;
+      }
     }
     this.data[0] = val;
     return this.data;
