2cc7a41b909aa3cbe3da85989498245486478a92
GavinC <gavincoulson1@gmail.com>
2023-03-19 15:40:52 -0600

    fun time with python orm

--- python_training/complete_fast_api/app/main.py
+++ python_training/complete_fast_api/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Response, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException, Depends
 from fastapi.params import Body
 from pydantic import BaseModel
 from typing import Optional
@@ -6,7 +6,17 @@ from random import randrange
 import psycopg2
 from psycopg2.extras import RealDictCursor
 import time
+from . import models
+from sqlalchemy.orm import Session
+from .database import engine, SessionLocal, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
 app = FastAPI()
+
+
+# Dependency
+# The session is what is ultimately responsible for talking with the DB
 
 
 class Post(BaseModel):
@@ -15,6 +25,7 @@ class Post(BaseModel):
     # if user doesn't provide published then it will default to true
     published: bool = True
     # rating: Optional[float] = None
+
 
 # don't have to do this
 # class UpdatePost(BaseModel):
@@ -27,7 +38,12 @@ class Post(BaseModel):
 while True:
     try:
         conn = psycopg2.connect(
-            host="localhost", database="fastapi", user="postgres", password="", cursor_factory=RealDictCursor)
+            host="localhost",
+            database="fastapi",
+            user="postgres",
+            password="",
+            cursor_factory=RealDictCursor,
+        )
         cursor = conn.cursor()
         print("Database connection was successful")
         break
@@ -38,10 +54,18 @@ while True:
 
 
 my_posts = [
-    {"title": "title of post 1",
-     "content": "content of post 1", "published": False, "id": 1},
-    {"title": "title of post 2",
-     "content": "content of post 2", "published": False, "id": 2}
+    {
+        "title": "title of post 1",
+        "content": "content of post 1",
+        "published": False,
+        "id": 1,
+    },
+    {
+        "title": "title of post 2",
+        "content": "content of post 2",
+        "published": False,
+        "id": 2,
+    },
 ]
 
 
@@ -49,21 +73,26 @@ my_posts = [
 async def root():
     return {"message": "Hello World!!"}
 
-# passing in an array like this is possible in fastapi as it will automaticaly serialize it and put it into JSON format so that it can be sent by the server and received by the client
+
+# little test route for understanding how the orm works
+@app.get("/sqlalchemy")
+def test_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return {"data": posts}
 
 
 @app.get("/posts")
-def get_posts():
-    cursor.execute("""SELECT * FROM posts""")
-    posts = cursor.fetchall()
-    print(posts)
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
     return {"data": posts}
 
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
-                   (post.title, post.content, post.published))
+    cursor.execute(
+        """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """,
+        (post.title, post.content, post.published),
+    )
     new_post = cursor.fetchone()
     conn.commit()
     return {"data": new_post}
@@ -71,10 +100,12 @@ def create_posts(post: Post):
 
 @app.get("/posts/latest")
 def get_latest():
-    post = my_posts[len(my_posts)-1]
+    post = my_posts[len(my_posts) - 1]
+
     return {"detail": post}
 
 
+# converting it back and forth to prevent a user from giving some random id, works for this but not necessarily a uuid which would be a string
 @app.get("/posts/{post_id}")
 def get_post_by_id(post_id: int):
     cursor.execute("""SELECT * FROM posts WHERE id = %s """, (str(post_id),))
@@ -82,19 +113,22 @@ def get_post_by_id(post_id: int):
     print(found_post)
 
     if found_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {post_id} was not found")
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id {post_id} was not found",
+        )
     return {"post": found_post}
 
 
 @app.delete("/posts/{post_id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id: int):
-    cursor.execute(
-        """DELETE FROM posts WHERE id = %s RETURNING *""", (str(post_id),))
+    cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(post_id),))
     deleted_post = cursor.fetchone()
     if deleted_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f'Post with id {post_id} was not found')
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Post with id {post_id} was not found",
+        )
     conn.commit()
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -102,11 +136,15 @@ def delete_post(post_id: int):
 @app.put("/posts/{post_id}")
 def update_post(update_post: Post, post_id: int):
     cursor.execute(
-        """UPDATE posts SET title = %s, content = %s, published =%s RETURNING *""", (update_post.title, update_post.content, update_post.published))
+        """UPDATE posts SET title = %s, content = %s, published =%s WHERE id = %s RETURNING *""",
+        (update_post.title, update_post.content, update_post.published, str(post_id)),
+    )
+
     updated_post = cursor.fetchone()
     if updated_post == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"post with id {post_id} was not found")
-
-    my_posts[index] = post_Dict
-    return {"data": post_Dict}
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"post with id {post_id} was not found",
+        )
+    conn.commit()
+    return {"data": updated_post}
---
+++ python_training/complete_fast_api/app/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.sql.expression import text
+from .database import Base
+from sqlalchemy.sql.sqltypes import TIMESTAMP
+
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    title = Column(String, nullable=False)
+    content = Column(String, nullable=False)
+    published = Column(Boolean, server_default="True", nullable=False)
+    created_at = Column(
+        TIMESTAMP(timezone=True), nullable=False, server_default=text("now()")
+    )
