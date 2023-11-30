97031b3e2c0345c1aae9da107d7816b916203acd
JetimLee <gavincoulson1@gmail.com>
2023-11-30 09:50:37 -0800

    printing variables, cool stuff

--- dart-flutter-learning/hello-world/bin/hello_world.dart
+++ dart-flutter-learning/hello-world/bin/hello_world.dart
@@ -2,4 +2,16 @@ import 'package:hello_world/hello_world.dart' as hello_world;
 
 void main(List<String> arguments) {
   print('Hello world: ${hello_world.calculate()}!');
+  String name = 'Gavin';
+  print("dart is awesome!");
+  print('$name is the best guy!');
+
+  String firstName = 'Andrew';
+  String lastName = 'Bizzoto';
+  int age = 36;
+  double height = 1.84;
+  print(firstName);
+  print(lastName);
+  print(age);
+  print(height);
 }
