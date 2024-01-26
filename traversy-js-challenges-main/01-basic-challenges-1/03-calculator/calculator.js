bef0d46e51ee6f9fd713d69b62a92af341ff9ffd
JetimLee <gavincoulson1@gmail.com>
2024-01-25 19:06:44 -0500

    calculator

--- traversy-js-challenges-main/01-basic-challenges-1/03-calculator/calculator.js
+++ traversy-js-challenges-main/01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,14 @@
-function calculator() {}
+function calculator(num1, num2, operator) {
+  if (operator === "/") {
+    return num1 / num2;
+  } else if (operator === "+") {
+    return num1 + num2;
+  } else if (operator === "-") {
+    return num1 - num2;
+  }
+  if (operator === "*") {
+    return num1 * num2;
+  }
+}
 
 module.exports = calculator;
