907ded49ea60e90f44cc864fe8d4fdfebd3ba948
JetimLee <gavincoulson1@gmail.com>
2024-01-25 19:46:33 -0500

    fun one

--- traversy-js-challenges-main/01-basic-challenges-1/06-title-case/title-case.js
+++ traversy-js-challenges-main/01-basic-challenges-1/06-title-case/title-case.js
@@ -1,3 +1,10 @@
-function titleCase() {}
+function titleCase(string) {
+  return string
+    .split(" ")
+    .map((word) => {
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join(" ");
+}
 
 module.exports = titleCase;
