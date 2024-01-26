6e780923bac2b0c31a7305199255216d46361a81
JetimLee <gavincoulson1@gmail.com>
2024-01-25 19:39:27 -0500

    another one

--- traversy-js-challenges-main/01-basic-challenges-1/05-find-max-number/find-max-number.js
+++ traversy-js-challenges-main/01-basic-challenges-1/05-find-max-number/find-max-number.js
@@ -1,5 +1,16 @@
+// function findMaxNumber(listOfNumbers) {
+//   return listOfNumbers.sort((a, b) => b - a)[0];
+// }
+
 function findMaxNumber(listOfNumbers) {
-  return listOfNumbers.sort((a, b) => b - a)[0];
+  let maxNumber = Math.min(-Infinity);
+
+  for (let i = 0; i < listOfNumbers.length; i++) {
+    if (listOfNumbers[i] > maxNumber) {
+      maxNumber = listOfNumbers[i];
+    }
+  }
+  return maxNumber;
 }
 
 module.exports = findMaxNumber;
