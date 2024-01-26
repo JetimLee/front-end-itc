98dfad985cc8eb0beb269b84cf1efd3fb587f572
JetimLee <gavincoulson1@gmail.com>
2024-01-25 19:20:56 -0500

    more fun

--- traversy-js-challenges-main/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
+++ traversy-js-challenges-main/01-basic-challenges-1/04-count-occurrences/count-occurrences.js
@@ -1,3 +1,11 @@
-function countOccurrences() {}
+function countOccurrences(string, char) {
+  let counter = 0;
+  for (let s of string) {
+    if (s === char) {
+      counter += 1;
+    }
+  }
+  return counter;
+}
 
 module.exports = countOccurrences;
