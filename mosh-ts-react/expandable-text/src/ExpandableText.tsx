378138bdf4204cc277df7b0c89547898a3e364ec
JetimLee <gavincoulson1@gmail.com>
2024-02-12 15:24:53 -0500

    simpler

--- mosh-ts-react/expandable-text/src/ExpandableText.tsx
+++ mosh-ts-react/expandable-text/src/ExpandableText.tsx
@@ -6,28 +6,19 @@ interface Props {
 }
 
 export const ExpandableText = ({ maxLimit = 100, children }: Props) => {
-  const [textToShow, setTextToShow] = useState<string>("");
   const [showingMore, setShowingMore] = useState<boolean>(false);
-
+  const text = showingMore ? children : children.substring(0, maxLimit);
   const handleSetTextToShow = () => {
-    const newText = children.slice(0, maxLimit) + "...";
-    setTextToShow(newText);
     setShowingMore(false);
   };
 
   const handleShowMore = () => {
-    setTextToShow(children);
     setShowingMore(true);
   };
 
-  // useEffect to run handleSetTextToShow on component mount and whenever the props change
-  useEffect(() => {
-    handleSetTextToShow();
-  }, [children, maxLimit]);
-
   return (
     <div>
-      <span>{textToShow}</span>
+      <span>{text}</span>
       {showingMore ? (
         <button onClick={handleSetTextToShow}>Show less</button>
       ) : (
